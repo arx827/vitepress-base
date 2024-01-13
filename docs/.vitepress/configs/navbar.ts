f6869c6306804093955298e8a4d6318bda144fb4
Eason Chen <arx827@gmail.com>
2024-01-14 03:34:14 +0800

    [feature] 『init』

---
+++ docs/.vitepress/configs/navbar.ts
@@ -0,0 +1,42 @@
+export const nav = [
+  {
+    text: '指南',
+    items: [
+      {
+        text: '介紹',
+        items: [
+          {
+            text: '什麼是VitePress',
+            link: '/page/guide/what-is-vitepress'
+          },
+          {
+            text: '快速上手',
+            link: '/page/guide/getting-started'
+          },
+          {
+            text: '配置',
+            link: '/page/guide/configuration'
+          },
+          // {
+          //   text: '靜態資源處理',
+          //   link: ''
+          // },
+          // {
+          //   text: 'Markdown擴展',
+          //   link: ''
+          // },
+          // {
+          //   text: '在Markdown使用Vue',
+          //   link: ''
+          // },
+          {
+            text: '部署',
+            link: '/page/guide/deploy'
+          },
+        ]
+      },
+      
+    ]
+  },
+]
+
